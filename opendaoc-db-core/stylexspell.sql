03837369116e36f00feb7d166687d9a1f2211b2f
Baptiste Marie <bm01@users.noreply.github.com>
2025-09-25 00:37:10 +0200

    Implement Savage's parry and evade style buffs

--- opendaoc-db-core/stylexspell.sql
+++ opendaoc-db-core/stylexspell.sql
@@ -1,9 +1,12 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 CREATE TABLE IF NOT EXISTS `stylexspell` (
   `StyleXSpell_ID` varchar(255) NOT NULL DEFAULT '',
   `SpellID` int(11) NOT NULL DEFAULT 0,
@@ -15,7 +18,6 @@ CREATE TABLE IF NOT EXISTS `stylexspell` (
   KEY `I_StyleXSpell_StyleID` (`StyleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
-/*!40000 ALTER TABLE `stylexspell` DISABLE KEYS */;
 REPLACE INTO `stylexspell` (`StyleXSpell_ID`, `SpellID`, `ClassID`, `StyleID`, `Chance`, `LastTimeRowUpdated`) VALUES
 	('00a57659-789c-47ed-9dfb-dc00adb384e7', 20206, 0, 347, 100, '2021-07-09 17:13:57'),
 	('03a72dc4-fe62-48a3-ac07-5cf4b9eaf69a', 20124, 0, 392, 100, '2021-07-09 21:02:41'),
@@ -258,12 +260,16 @@ REPLACE INTO `stylexspell` (`StyleXSpell_ID`, `SpellID`, `ClassID`, `StyleID`, `
 	('savage5', 20001, 0, 376, 100, '2000-01-01 00:00:00'),
 	('savage6', 20033, 0, 377, 100, '2000-01-01 00:00:00'),
 	('savage7', 20301, 0, 378, 100, '2000-01-01 00:00:00'),
-	('SavageBuffParryStyle', 10506, 0, 381, 100, '2000-01-01 00:00:00'),
+	('SavageBuffParryStyle', 10499, 0, 381, 100, '2000-01-01 00:00:00'),
 	('SavageBuffStyleEvade', 10500, 0, 380, 100, '2000-01-01 00:00:00'),
 	('slam', 20407, 0, 228, 100, '2021-07-14 17:10:28'),
 	('totemic sacrifice multi', 991342, 32, 384, 100, '2000-01-01 00:00:00'),
 	('totemic wrath multi', 991341, 32, 379, 100, '2000-01-01 00:00:00'),
 	('tribal assault multi', 991340, 32, 374, 100, '2000-01-01 00:00:00'),
 	('zz0198bf-d628-4a5e-9ab1-1396acff21ed', 20001, 0, 200, 100, '2021-07-11 11:58:47');
-/*!40000 ALTER TABLE `stylexspell` ENABLE KEYS */;
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
